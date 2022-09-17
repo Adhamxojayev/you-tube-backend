fb8d12f2b02f63e43c73cff6fc43a70211ddccf7
Ahror Adhamxojayev <axroradhamxojayev@gmail.com>
2022-09-17 20:41:53 +0500

    init

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { VideoModule } from './video/video.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      database: process.env.POSTGRES_DATABASE,
+      password: process.env.POSTGRES_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+    UserModule,
+    VideoModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
